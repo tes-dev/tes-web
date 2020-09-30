93db427cc1aa97a7c040a8da9f3a88c3baed2c23
maxwell <zhong.maxwell@gmail.com>
2020-09-30 10:50:00 +0800

    更新参加评教页面

--- src/views/ems/eval/index.vue
+++ src/views/ems/eval/index.vue
@@ -1,13 +1,57 @@
 <template>
-  <h1>这是开始评教页面</h1>
+  <div class="app-container">
+    <div class="table-container">
+      <el-table
+        ref="evalTable"
+        :data="evalList"
+        style="width: 100%"
+        v-loading="listLoading"
+        border
+      >
+        <el-table-column label="编号" width="100" align="center" class="type">
+          <!-- <template slot-scope="scope">{{ scope.row.id }}</template> -->
+        </el-table-column>
+        <el-table-column label="课程id" align="center">
+          <template slot-scope="scope">{{ scope.row.courseId }}</template>
+        </el-table-column>
+        <el-table-column label="课程名" align="center">
+          <template slot-scope="scope">{{ scope.row.courseName }}</template>
+        </el-table-column>
+        <el-table-column label="教师" align="center">
+          <template slot-scope="scope">{{ scope.row.targetName }}</template>
+        </el-table-column>
+        <el-table-column label="班级" align="center">
+          <template slot-scope="scope">{{ scope.row.classNo }}</template>
+        </el-table-column>
+        <el-table-column label="院系" align="center">
+          <template slot-scope="scope">{{ scope.row.deptName }}</template>
+        </el-table-column>
+        <el-table-column label="学期" align="center">
+          <template slot-scope="scope">{{ scope.row.semesterName }}</template>
+        </el-table-column>
+        <el-table-column label="操作" width="180" align="center">
+          <template slot-scope="scope">
+            <el-button
+              size="mini"
+              type="text"
+              @click="handleEval(scope.$index, scope.current - row)"
+            >
+              开始评教
+            </el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "anal",
+  name: "eval",
   data() {
     return {
-
+      listLoading: false,
+      evalList: null,
     }
   },
   created() {
