d8e5466e3679c69128dbae9765aed64c1d83a9f7
yangli <434492558@qq.com>
2020-10-20 08:33:49 +0800

    添加班级管理搜索框

--- src/views/lms/class/index.vue
+++ src/views/lms/class/index.vue
@@ -1,5 +1,43 @@
 <template>
   <div class="app-container">
+    <el-card class="filter-container" shadow="never">
+      <div>
+        <i class="el-icon-search"></i>
+        <span>筛选搜索</span>
+        <el-button
+          style="float: right"
+          type="primary"
+          @click="handleSearchList()"
+          size="small"
+        >
+          查询搜索
+        </el-button>
+        <el-button
+          style="float: right; margin-right: 15px"
+          @click="handleResetSearch()"
+          size="small"
+        >
+          重置
+        </el-button>
+      </div>
+      <div style="margin-top: 15px">
+        <el-form
+          :inline="true"
+          :model="listQuery"
+          size="small"
+          label-width="140px"
+        >
+          <el-form-item label="输入搜索：">
+            <el-input
+              v-model="listQuery.keyword"
+              class="input-width"
+              placeholder="班级名称"
+              clearable
+            ></el-input>
+          </el-form-item>
+        </el-form>
+      </div>
+    </el-card>
     <el-card class="operate-container" shadow="never">
       <i class="el-icon-tickets" style="margin-top: 5px"></i>
       <span style="margin-top: 5px">数据列表</span>
@@ -236,6 +274,13 @@ export default {
         this.deptnoArr = response.data
       })
     },
+    handleResetSearch() {
+      this.listQuery = Object.assign({}, defaultListQuery)
+    },
+    handleSearchList() {
+      this.listQuery.pageNum = 1
+      this.getList()
+    }
   
   }
 }
